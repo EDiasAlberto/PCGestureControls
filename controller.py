7822bd89e6114c51d5b2000f017c05f138778bc5
EDiasAlberto <retr0fade111@gmail.com>
2025-08-25 22:00:00 +0100

    feat: move screen size calculation out of movement function

--- controller.py
+++ controller.py
@@ -5,10 +5,10 @@ class Controller:
         pag.FAILSAFE = False  # Disable the fail-safe feature
         self.isClicking = False
         self.isRightClicking = False
+        self.screenWidth, self.screenHeight = pag.size()
 
     def move_cursor(self, x, y):
-        screenWidth, screenHeight = pag.size()
-        pag.moveTo(((1 - x) * screenWidth), y * screenHeight, duration=0.1, tween=pag.linear)
+        pag.moveTo(((1 - x) * self.screenWidth), y * self.screenHeight, duration=0.1, tween=pag.linear)
 
     def mouseDown(self):
         if (self.isClicking):
