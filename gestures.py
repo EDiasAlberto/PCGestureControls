0daf20feb763bad9cd56b7af36801a1e8f9c1bc4
EDiasAlberto <proxamon1@gmail.com>
2025-08-26 02:07:26 +0100

    feat: implement multithreading

--- gestures.py
+++ gestures.py
@@ -1,6 +1,7 @@
 import cv2
 from math import hypot
 from enum import Enum
+import platform 
 import mediapipe as mp
 import numpy as np
 mp_drawing = mp.solutions.drawing_utils
@@ -25,7 +26,11 @@ class GestureTracking:
 
     def __init__(self, positionHandler, camera_index=0, detection_confidence=0.5, tracking_confidence=0.5):
         self.isPinching = False
-        self.cap = cv2.VideoCapture(camera_index)
+        # Use platform-appropriate VideoCapture
+        if platform.system() == "Windows":
+            self.cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
+        else:
+            self.cap = cv2.VideoCapture(camera_index)
         self.hands = mp_hands.Hands(
             model_complexity=0,
             max_num_hands=1,
@@ -52,45 +57,17 @@ class GestureTracking:
             success, image = self.cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
                 continue
 
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             results = self.hands.process(image)
 
-            # Draw the hand annotations on the image.
-            if isDrawing:
-                image.flags.writeable = True
-                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                if results.multi_hand_landmarks:
-                    hand_landmarks = results.multi_hand_landmarks[0]
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
-
-                    
             if results.multi_hand_landmarks:
                 self.landmarks = results.multi_hand_landmarks[0].landmark
                 self.isPinching = self.distance(self.landmarks[THUMB_TIP], self.landmarks[INDEX_TIP]) < 0.05
                 self.isSecondaryPinching = self.distance(self.landmarks[THUMB_TIP], self.landmarks[PINKY_TIP]) < 0.05
                 self.palmPosition = self.average_pos()
                 self.positionHandler(self.palmPosition[0], self.palmPosition[1], self.isPinching, self.isSecondaryPinching)
-            
-            if isDrawing:
-                # draw a circle at the palm position
-                h, w, c = image.shape
-                cv2.circle(image, (int(self.palmPosition[0] * w), int(self.palmPosition[1] * h)), 10, (255, 0, 0), -1)
-            
-            # Flip the image horizontally for a selfie-view display.
-            if isDrawing:
-                cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
         self.cap.release()
 
--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from gestures import GestureTracking
 from controller import Controller
+import threading
 
 controller = Controller()
 
+X_COORD = 0
+Y_COORD = 0
+IS_PINCHING = False
+IS_SECONDARY_PINCHING = False
 
-def gestures_position_handler(x, y, isPinching, isSecondaryPinching):
-    controller.move_cursor(x, y)
-    if isPinching:
-        controller.mouseDown()
-    else:
-        controller.mouseUp()
-    
-    if isSecondaryPinching:
-        controller.rightClickDown()
-    else:
-        controller.rightClickUp()
+def update_coords(x, y, isPinching, isSecondaryPinching):
+    global X_COORD, Y_COORD, IS_PINCHING, IS_SECONDARY_PINCHING
+    X_COORD = x
+    Y_COORD = y
+    IS_PINCHING = isPinching
+    IS_SECONDARY_PINCHING = isSecondaryPinching
+
+def move_mouse():
+    while True:
+        controller.move_cursor(X_COORD, Y_COORD)
+        if IS_PINCHING:
+            controller.mouseDown()
+        else:
+            controller.mouseUp()
+
+        if IS_SECONDARY_PINCHING:
+            controller.rightClickDown()
+        else:
+            controller.rightClickUp()
 
 def main():
-    gestures = GestureTracking(gestures_position_handler)
-    gestures.run(False)
+    gestures = GestureTracking(update_coords)
+    t1 = threading.Thread(target=gestures.run)
+    t2 = threading.Thread(target=move_mouse)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 
 if __name__ == "__main__":
